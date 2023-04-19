d99868f6e29c7f8c100c628274750df26feb4105
maxai <maxai@MAX>
2023-04-19 18:53:46 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first name: ");
+            string fn = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter last name: ");
+            string ln = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter year: ");
+            string y = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter course: ");
+            string c = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter section: ");
+            string s = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter midterm grade: ");
+            float mg = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Enter final grade: ");
+            float fg = Convert.ToSingle(Console.ReadLine());
+
+
+
+            Student record = new Student(fn,ln,y,c,s,mg,fg);
+            record.introduceSelf();
+            Console.WriteLine();
+            record.evaluateGrade();
+        }
+    }
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        string firstName;
+        string lastName;
+        string year;
+        string course;
+        string section;
+        float midtermGrade;
+        float finalGrade;
+
+        public Student(string firstName, string lastName, string year, string course, string section, float midtermGrade, float finalGrade)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.year = year;
+            this.course = course;
+            this.section = section;
+            this.midtermGrade = midtermGrade;
+            this.finalGrade = finalGrade;
+        }
+
+        public void introduceSelf()
+        {
+            Console.WriteLine("Full Name: " + firstName + " " + lastName);
+            Console.WriteLine("Course: " + course);
+            Console.WriteLine("Year: " + year);
+            Console.WriteLine("Section: " + section);
+        }
+
+        public void evaluateGrade()
+        {
+            float average = (midtermGrade + finalGrade) / 2;
+            if(average >= 90) Console.WriteLine("Grade: " + average + " Honor");
+
+            else if (average >= 75 ) Console.WriteLine("Grade: " + average + " Passed");
+
+            else Console.WriteLine("Fail");
+
+
+
+        }
+    }
+}
